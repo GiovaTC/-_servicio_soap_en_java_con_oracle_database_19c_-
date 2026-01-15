b9c906e5d9b8ba403055f4edada76f4587b25be9
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-15 13:23:05 -0500

    commit - AvionService .

--- src/avion/AvionService.java
+++ src/avion/AvionService.java
@@ -1,6 +1,22 @@
 package avion;
 
 import javax.jws.WebMethod;
+import javax.jws.WebService;
 
+@WebService
 public class AvionService {
+
+    @WebMethod
+    public String registrarAvion(
+            String modelo,
+            String fabricante,
+            int capacidad,
+            int autonomia) {
+
+        Avion avion = new Avion(modelo, fabricante, capacidad, autonomia);
+        AvionDAO dao = new AvionDAO();
+        dao.registrarAvion(avion);
+
+        return "avion registrado correctamente en oracle 19C";
+    }
 }
