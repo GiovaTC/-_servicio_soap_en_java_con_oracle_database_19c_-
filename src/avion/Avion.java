c944310a2d60cc73d4ffd1decc3a21e97d8c5b69
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-15 12:36:36 -0500

    commit - Avion / .

--- src/avion/Avion.java
+++ src/avion/Avion.java
@@ -1,4 +1,52 @@
 package avion;
 
-public class Avion {
+import java.io.Serializable;
+
+public class Avion implements Serializable {
+
+    private String modelo;
+    private String fabricante;
+    private int capacidad;
+    private int autonomia;
+
+    public Avion() {}
+
+    public Avion(String modelo, String fabricante, int capacidad, int autonomia) {
+        this.modelo = modelo;
+        this.fabricante = fabricante;
+        this.capacidad = capacidad;
+        this.autonomia = autonomia;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getAutonomia() {
+        return autonomia;
+    }
+
+    public void setAutonomia(int autonomia) {
+        this.autonomia = autonomia;
+    }
 }
